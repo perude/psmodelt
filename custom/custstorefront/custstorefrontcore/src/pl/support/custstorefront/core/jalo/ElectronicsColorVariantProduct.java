a4df53d11c5c312f06fb04cad5da61dc4a52876d
perude <marek.dec@sap.com>
2020-07-16 12:15:02 +0200

    new version

--- custom/custstorefront/custstorefrontcore/src/pl/support/custstorefront/core/jalo/ElectronicsColorVariantProduct.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
- */
-package pl.support.custstorefront.core.jalo;
-
-public class ElectronicsColorVariantProduct extends GeneratedElectronicsColorVariantProduct
-{
-	// Deliberately empty class
-}
